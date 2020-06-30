bd4f56982b69853f4c27b57893e211e77a6112fb
Vladuxexel <55832677+Vladuxexel@users.noreply.github.com>
2020-06-30 15:55:16 +0700

    Setting dataContext and getting coordinates by click

--- Primitives/MainWindow.xaml.cs
+++ Primitives/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Primitives
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowVM(Viewport);
         }
     }
 }
---
+++ Primitives/MainWindowVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using SharpDX;
+using HelixToolkit.Wpf;
+
+namespace Primitives
+{
+    class MainWindowVM
+    {
+        private HelixViewport3D viewport;
+
+        public double x, y;
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public MainWindowVM(HelixViewport3D viewport)
+        {
+            this.viewport = viewport;
+            viewport.MouseDown += OnMouseClick;
+        }
+
+        public Viewport MainViewport { get; set; }
+
+        public void OnMouseClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show(viewport.CursorPosition.ToString());
+        }
+    }
+}

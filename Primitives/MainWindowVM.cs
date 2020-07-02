51d39f5ad5879ef0fb103cb5caf875ba0f51ed78
Vladuxexel <55832677+Vladuxexel@users.noreply.github.com>
2020-07-02 18:04:26 +0700

    Drawing a square by mouseClicking

--- Primitives/MainWindowVM.cs
+++ Primitives/MainWindowVM.cs
@@ -11,18 +11,38 @@ using MVVM;
 using System.Windows.Media.Media3D;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace Primitives
 {
-    class MainWindowVM
+    class MainWindowVM : INotifyPropertyChanged
     {
         private HelixViewport3D viewport;
+        /// <summary>
+        /// Gets or sets the model.
+        /// </summary>
+        /// <value>The model.</value>
+        Model3D model;
+        public Model3D Model
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+                OnPropertyChanged("Model");
+            }
+        }
 
         public MainWindowVM(HelixViewport3D viewport) : this()
         {
             this.viewport = viewport;
         }
         private RelayCommand drawCommand;
+
+        [Obsolete]
         public RelayCommand DrawCommand
         {
             get
@@ -30,39 +50,69 @@ namespace Primitives
                 return drawCommand ??
                     (drawCommand = new RelayCommand(obj =>
                     {
-                        MessageBox.Show(viewport.CursorPosition.ToString());
+                        // Create a model group
+                        var modelGroup = new Model3DGroup();
+
+                        // Create a mesh builder and add a box to it
+                        var meshBuilder = new MeshBuilder(false, false);
+                        meshBuilder.AddBox(new Point3D(coordinate('X'), coordinate('Y'), 0), 0.1, 0.1, 0);
+
+                        // Create a mesh from the builder (and freeze it)
+                        var mesh = meshBuilder.ToMesh(true);
+
+                        // Create some materials
+                        var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
+
+                        // Add 3 models to the group (using the same mesh, that's why we had to freeze it)
+                        modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Material = greenMaterial });
+
+                        // Set the property, which will be bound to the Content property of the ModelVisual3D (see MainWindow.xaml)
+                        this.Model = modelGroup;
                     }));
             }
         }
 
         public MainWindowVM()
         {
-            // Create a model group
-            var modelGroup = new Model3DGroup();
-
-            // Create a mesh builder and add a box to it
-            var meshBuilder = new MeshBuilder(false, false);
-            meshBuilder.AddBox(new Point3D(0, 0, 1), 1, 2, 0.5);
-            meshBuilder.AddBox(new Rect3D(0, 0, 1.2, 0.5, 1, 0.4));
-
-            // Create a mesh from the builder (and freeze it)
-            var mesh = meshBuilder.ToMesh(true);
-
-            // Create some materials
-            var greenMaterial = MaterialHelper.CreateMaterial(Colors.Green);
-            var insideMaterial = MaterialHelper.CreateMaterial(Colors.Yellow);
-
-            // Add 3 models to the group (using the same mesh, that's why we had to freeze it)
-            modelGroup.Children.Add(new GeometryModel3D { Geometry = mesh, Material = greenMaterial, BackMaterial = insideMaterial });
-
-            // Set the property, which will be bound to the Content property of the ModelVisual3D (see MainWindow.xaml)
-            this.Model = modelGroup;
+            
         }
 
-        /// <summary>
-        /// Gets or sets the model.
-        /// </summary>
-        /// <value>The model.</value>
-        public Model3D Model { get; set; }
+        [Obsolete]
+        private double coordinate(char name)
+        {
+            int index = 0;
+            double result = 0;
+            string xyz = viewport.CurrentPosition.ToString();
+
+            switch (name)
+            {
+                case 'X':
+                    index = xyz.IndexOf(';');
+                    xyz = xyz.Substring(0, index);
+                    result = Convert.ToDouble(xyz);
+                    break;
+                case 'Y':
+                    index = xyz.IndexOf(';');
+                    xyz = xyz.Substring(index + 1);
+                    index = xyz.IndexOf(';');
+                    xyz = xyz.Substring(0, index);
+                    result = Convert.ToDouble(xyz);
+                    break;
+                case 'Z':
+                    index = xyz.IndexOf(';');
+                    xyz = xyz.Substring(index + 1);
+                    index = xyz.IndexOf(';');
+                    xyz = xyz.Substring(index + 1);
+                    result = Convert.ToDouble(xyz);
+                    break;
+            }
+            return result;
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
